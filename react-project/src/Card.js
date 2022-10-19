364685173aa6d8d4ea0e98d9ce9ba85450bf8674
rob <robert.magerl18@gmail.com>
2022-10-19 15:01:05 -0700

    Added CardList

--- react-project/src/Card.js
+++ react-project/src/Card.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-const Card = ({ id, name, email}) => {
+const Card = ({name, email, id}) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>   
-            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
@@ -11,5 +11,6 @@ const Card = ({ id, name, email}) => {
         </div>
     )
 }
+
 
 export default Card;
